1b79a3a5315fce03d99c16f19d530385c05b2ded
uhzero <aureo.moraes.06@gmail.com>
2023-05-13 11:05:25 -0300

    add buttons to forms

--- src/components/financas/Formulario.tsx
+++ src/components/financas/Formulario.tsx
@@ -1,5 +1,9 @@
+import "dayjs/locale/pt-br";
 import Transacao from "@/logic/core/financas/Transacao";
-import { TextInput } from "@mantine/core";
+import Dinheiro from "@/logic/utils/Dinheiro";
+import { Button, Group, Radio, TextInput } from "@mantine/core";
+import { DatePickerInput } from "@mantine/dates";
+import { TipoTransacao } from "@/logic/core/financas/TipoTransacao";
 
 interface FormularioProps {
     transacao: Transacao
@@ -17,6 +21,46 @@ export default function Formulario(props: FormularioProps) {
                     label="Descrição"
                     value={props.transacao.descricao}
                 />
+                <TextInput
+                    label="Valor"
+                    value={Dinheiro.formatar(props.transacao.valor)}
+                />
+                <DatePickerInput
+                    label="Data"
+                    value={props.transacao.data}
+                    locale="pt-BR"
+                    valueFormat="DD/MM/YYYY"
+                />
+                <Radio.Group
+                    value={props.transacao.tipo}
+                    onChange={() => {}}
+                >
+                    <Group>
+                        <Radio value={TipoTransacao.RECEITA} label="Receita" />
+                        <Radio value={TipoTransacao.DESPESA} label="Despesa" />
+                    </Group>
+                </Radio.Group>
+            </div>
+            <div className="flex px-4 sm:px-7 py-4 gap-3 bg-zinc-800">
+                <Button
+                    className="bg-green-500" color="green"
+                >
+                    Salvar
+                </Button>
+                <Button
+                    className="bg-zinc-500" color="gray"
+                >
+                    Voltar
+                </Button>
+                <div className="flex-1"></div>
+                {props.transacao.id && (
+                    <Button
+                        className="bg-red-500"
+                        color="red"
+                    >
+                        Excluir
+                    </Button>
+                )}
             </div>
         </div>
     )
--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -10,13 +10,14 @@ import Formulario from "./Formulario";
 export default function Financas() {
 
     const [transacoes, setTransacoes] = useState<Transacao[]>(fakeTransactions)
+    const [transacao, settransacao] = useState<Transacao | null>(null)
 
     return (
         <Pagina>
             <Cabecalho />
             <Conteudo className="gap-5">
-                <Lista transacoes={transacoes} />
-                <Formulario transacao={transacoes[0]} />
+                <Lista transacoes={transacoes} selecionarTransacao={settransacao} />
+                {transacao && <Formulario transacao={transacao} />}
             </Conteudo>
         </Pagina>
     )
